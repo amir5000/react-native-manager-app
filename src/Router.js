57cfac7e36ccad7a5bbd254b3099e3267f293048
aarafat <amirarafat1@gmail.com>
2016-12-17 15:23:03 -0700

    added Router File

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene />
+    </Router>
+  )
+}
+
+export default RouterComponent;
