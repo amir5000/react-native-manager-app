230046a03d059e73b03826ce1564c74c907fda73
aarafat <amirarafat1@gmail.com>
2016-12-19 17:02:19 -0700

    added employee list

---
+++ src/components/EmploeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Employess List</Text>
+        <Text>Employess List</Text>
+        <Text>Employess List</Text>
+        <Text>Employess List</Text>
+        <Text>Employess List</Text>
+        <Text>Employess List</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
