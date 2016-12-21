b9653fe786c6d5da3de2d462ee1ce2371022bf7c
aarafat <amirarafat1@gmail.com>
2016-12-21 11:16:59 -0700

    created new scene layout for app

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ import {
   LOGIN_USER
 } from './types';
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 
 export const emailChanged = (text) => {
   return {
@@ -43,4 +44,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.main();
 }
