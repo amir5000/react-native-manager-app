d7be3f96372e2c712808115814f6261f36707b4b
aarafat <amirarafat1@gmail.com>
2016-12-22 14:21:19 -0700

    created new create employee scene and reducer/action creator cleanup

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import Actions from 'react-native-router-flux';
 
 const RouterComponent = () => {
   return (
@@ -11,7 +13,15 @@ const RouterComponent = () => {
       </Scene>
 
       <Scene key="main">
-          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+          <Scene
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            initial
+          />
+        <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employess" />
       </Scene>
     </Router>
   )
