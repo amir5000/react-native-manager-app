78e8c071d79bd6b0291b708451a25f32e5f5af21
aarafat <amirarafat1@gmail.com>
2016-12-24 00:33:15 -0700

    reset the default values after adding the new employee

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
-import { EMPLOYEE_UPDATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -10,7 +11,13 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employee`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.employeeList({ type:'reset' })
+      });
+  }
 
-  firebase.database().ref(`/users/${currentUser.uid}/employee`)
-    .push({ name, phone, shift });
 };
