b7fe423e1d354e19bdfabea539b02662f15d1033
aarafat <amirarafat1@gmail.com>
2016-12-24 00:18:35 -0700

    added default value for picker

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import { EMPLOYEE_UPDATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -8,5 +9,8 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log({ name, phone, shift });
+  const { currentUser } = firebase.auth();
+
+  firebase.database().ref(`/users/${currentUser.uid}/employee`)
+    .push({ name, phone, shift });
 };
