484bba08088c8d0540cc86878ad587415b5a6573
aarafat <amirarafat1@gmail.com>
2017-03-19 21:46:29 -0600

    added employees list in props

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -5,8 +7,6 @@ import {
   LOGIN_USER_FAIL,
   LOGIN_USER
 } from './types';
-import firebase from 'firebase';
-import { Actions } from 'react-native-router-flux';
 
 export const emailChanged = (text) => {
   return {
@@ -25,12 +25,14 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
+		
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => loginUserSuccess(dispatch, user))
-    .catch(() => {
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => loginUserSuccess(dispatch, user))
-        .catch(() => loginUserFail(dispatch));
+    	.then(user => loginUserSuccess(dispatch, user))
+    	.catch((error) => {
+				console.log(error);
+      	firebase.auth().createUserWithEmailAndPassword(email, password)
+        	.then(user => loginUserSuccess(dispatch, user))
+        	.catch(() => loginUserFail(dispatch));
     });
   };
 };
@@ -40,10 +42,12 @@ const loginUserFail = (dispatch) => {
 };
 
 const loginUserSuccess = (dispatch, user) => {
-  dispatch({
-    type: LOGIN_USER_SUCCESS,
-    payload: user
-  });
-
-  Actions.main();
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	});
+	
+	console.log(user);
+	
+	Actions.main();
 }
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,25 +1,48 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
-  componentWillMount() {
-    this.props.employeeFetch();
-  }
+    componentWillMount() {
+        this.props.employeeFetch();
+        this.createDataSource(this.props);
+    }
 
-  render() {
-    return (
-      <View>
-        <Text>Employess List</Text>
-        <Text>Employess List</Text>
-        <Text>Employess List</Text>
-        <Text>Employess List</Text>
-        <Text>Employess List</Text>
-        <Text>Employess List</Text>
-      </View>
-    );
-  }
+    conponentWillRecieveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+    
+    createDataSource( { employees } ) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+    
+    render() {
+        console.log(this.state);
+        return (
+            <View>
+                <Text>Employess List</Text>
+                <Text>Employess List</Text>
+                <Text>Employess List</Text>
+                <Text>Employess List</Text>
+                <Text>Employess List</Text>
+                <Text>Employess List</Text>
+            </View>
+        );
+    }
 }
 
-export default connect(null, { employeeFetch })(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+       return { ...val, uid };
+    });
+    
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList);
