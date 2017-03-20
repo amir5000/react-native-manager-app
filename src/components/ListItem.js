ace7a38a1b4ab5d2a32e357fd9208ba37906c202
aarafat <amirarafat1@gmail.com>
2017-03-20 00:03:47 -0600

    list onPress added to edit employee

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { CardSection } from './common';
+import { Actions } from 'react-native-router-flux';
 
 class ListItem extends Component {
+	onRowPress() {
+		Actions.employeeCreate({ employee: this.props.employee });
+	}
+	
 	render() {
 		const { name } = this.props.employee;
-		console.log(name);
 		return (
-			<CardSection>
-				<Text style={styles.titleStyle}>{ name }</Text>
-			</CardSection>
+			<TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+				<View>
+					<CardSection>
+						<Text style={styles.titleStyle}>{ name }</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
